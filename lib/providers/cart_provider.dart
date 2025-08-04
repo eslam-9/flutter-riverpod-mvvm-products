ebb73cf755bf7ece522b9b5b3b0da626230bff65
eslam-9 <eslamage20@gmail.com>
2025-08-04 14:07:18 +0300

    use build runner

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,24 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_files/models/product.dart';
+
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
+  @override
+  Set<Product> build() {
+    return {};
+  }
+
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_files/providers/products_proviser.dart';
+import 'package:riverpod_files/providers/products_provider.dart';
 import 'package:riverpod_files/shared/cart_icon.dart';
 
 class HomeScreen extends ConsumerWidget {
