43fdb0963c16d7a7f36f27c6d43e2aafee365377
eslam-9 <eslamage20@gmail.com>
2025-08-04 10:55:15 +0300

    initial commit for Riverpod MVVM product app

---
+++ lib/providers/products_provider.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'products_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productHash() => r'5c51d823a6be6793e74f8ab336b893404e93b868';
+
+/// See also [product].
+@ProviderFor(product)
+final productProvider = AutoDisposeProvider<List<Product>>.internal(
+  product,
+  name: r'productProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$productHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ProductRef = AutoDisposeProviderRef<List<Product>>;
+String _$reducedProductsHash() => r'a3555e8c5d0951dd3f477459086a52acf7627484';
+
+/// See also [reducedProducts].
+@ProviderFor(reducedProducts)
+final reducedProductsProvider = AutoDisposeProvider<List<Product>>.internal(
+  reducedProducts,
+  name: r'reducedProductsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$reducedProductsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ReducedProductsRef = AutoDisposeProviderRef<List<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
