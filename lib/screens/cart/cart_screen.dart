321346dc403c042d22e56f5199504d21c03b6ae7
eslam-9 <eslamage20@gmail.com>
2025-08-04 19:27:44 +0300

    add localization

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_files/providers/products_proviser.dart';
+import 'package:riverpod_files/providers/cart_provider.dart';
 
 class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
@@ -14,7 +14,7 @@ class _CartScreenState extends ConsumerState<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final products = ref.watch(reducedProductsProvider);
+    final products = ref.watch(cartNotifierProvider);
 
     return Scaffold(
       appBar: AppBar(
